362ff791862064543773a2284f07ede0f40f1e77
catherine s <goldyflakes@gmail.com>
2023-07-05 21:50:23 -0400

    commented some frontend code

--- client/app/_components/test.tsx
+++ client/app/_components/test.tsx
@@ -18,15 +18,20 @@ export default function Test() {
             .then(out => setcurrentRoomId(out))
     }, [])
     
+    //fires whenever the user types in the input box
     function handleChange(event: ChangeEvent<HTMLInputElement>) {
         setInputText(event.target.value);
     }
     
+    //fires when the user presses enter on the input box
     const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault();
+        event.preventDefault(); //prevent default behaviour (reloading webpage)
         console.debug(`input: ${inputText}`);
+        
+        //log user command in output container
         setOutputText(processText(inputText, false));
         
+        //try to process user-entered command on the server
         fetch(ENDPOINT + "/api/test", {
             method: 'POST',
             credentials: 'include',
@@ -38,45 +43,54 @@ export default function Test() {
                 'Content-type': 'application/json'
             }
         })
-            .then(res => res.json())
-            .then(out => handleOutput(out));
+            .then(res => res.json()) //convert output to object
+            .then(out => handleOutput(out)); //handle server output
+            //TODO: handle errors
     }
     
+    //clears input text, changes roomID, and processess the output from the server
     function handleOutput(output: game_message) {
+        //TODO: handle errors
         setInputText(""); //clear input field
         setcurrentRoomId(output.roomId);
         setOutputText(processText(output.message, true));
     }
     
-    //update container with new dummy containers with text
+    //whenever we get new output, add it to the output container log
     useEffect(() => {
         const container = document.getElementById("output-container");
         if(!container) return;
         
+        //add new output to container
         if(outputText) container.appendChild(outputText);
+        
+        //scroll to bottom of scrollbox
         const scrollbox = container.parentElement;
         if(scrollbox) scrollbox.scrollTop = scrollbox.scrollHeight;
         
     }, [outputText])
     
-    //process text into a div
+    //process text into a div which can be added to the output container
     function processText(text: string, color: boolean): HTMLDivElement {
         if(color) 
             return colorText(text);
         
+        //if not coloring, assume it's input
         let container = document.createElement("div");
         
+        //pre is the two chevrons, ">>", used to denote user input
         let pre = document.createElement("div");
-        let div = document.createElement("div");
+        let input = document.createElement("div");
         
         container.appendChild(pre);
-        container.appendChild(div);
+        container.appendChild(input);
         
         pre.className = "inline text-green-400 font-semibold font-mono";
-        div.className = "inline font-mono";
+        input.className = "inline font-mono";
         
+        //newline to create a space between the previous log entry and the new one
         pre.textContent = "\n>> ";
-        div.textContent = text;
+        input.textContent = text;
         
         return container;
     }
@@ -84,12 +98,12 @@ export default function Test() {
     //color message using # as seperators
     function colorText(text: string): HTMLDivElement {
         let container = document.createElement("div");
-        //container.className="inline"
         
         text.split("#").forEach(t => {
-            console.log(`t: ${t}`);
             let div = document.createElement("div");
-            div.textContent = t;
+            
+            //the string to be colored, or the entire string if no colors exist
+            div.textContent = t; 
             div.className = "inline ";
             
             //check for color codes!
@@ -97,26 +111,27 @@ export default function Test() {
             switch (firstWord) {
                 case "red":
                     div.className += "text-red-400";
-                    div.textContent = rest.join(" ");
+                    div.textContent = rest.join(" "); //skip color code
                     break;
                     
                 case "cyan":
                     div.className += "text-cyan-400";
-                    div.textContent = rest.join(" ");
+                    div.textContent = rest.join(" "); //skip color code
                     break;
                     
                 case "green":
                     div.className += "text-green-400";
-                    div.textContent = rest.join(" ");
+                    div.textContent = rest.join(" "); //skip color code
                     break;
                     
                 case "yellow":
                     div.className += "text-yellow-400";
-                    div.textContent = rest.join(" ");
+                    div.textContent = rest.join(" "); //skip color code
                     break;
                     
                 default:
                     div.className += "text-white";
+                    //no color code to skip, assume white
                     break;
             }
             
