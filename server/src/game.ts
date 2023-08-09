e4286fc56efe6c451b01bfd40c74cf5a262556d4
catherine s <goldyflakes@gmail.com>
2023-08-09 15:35:59 -0400

    commenting some planning for rooms

--- server/src/game.ts
+++ server/src/game.ts
@@ -22,34 +22,41 @@ const start3 = new Room("The end of the path in a dense forest", "You come, fina
 start2.setExit("N", start3);
 start3.entities = [
     new SignEntity("Good job following basic instructions! Commands, like the ones you've been typing, are how you interact with the world of Kynopsia and its many diverse inhabitants - inhabitants you should be able to meet very soon! \nType #red list# to see the full list of commands, and #red help [command]# to get information about specific commands! You can also type #red help# on its own for some general guidance if you're confused! \n\nContinue North now - you're ready to join your friends and have countless adventures! Welcome to #yellow Kynopsia#!")
-    
 ]
 
 
 
-const cityEntrance = new Room("the city entrance", "yeah");
+const cityEntrance = new Room("Balial - Town Entrance", "yeah"); //introduce say and social aspects, lead to guild hall
 start3.setExit("N", cityEntrance);
 
-const guildHall = new Room("the guild hall", "yeah");
+
+const guildHall = new Room("Balial - Guild Hall", "yeah"); //empty feeling, lack of players. maybe expand to more than 1 room for believability
 cityEntrance.setExit("E", guildHall);
 
-const cityStore = new Room("the city store", "yeah");
+
+const cityStore = new Room("Balial - The Rusted Scale", "yeah"); //introduce aging server. npc not working? not content that is lead to
 cityEntrance.setExit("W", cityStore);
 
-const cityCenter = new Room("the city center", "yeah");
+
+
+const cityCenter = new Room("Balial - Town Center", "yeah"); //big! town message board, traces of previous events
 cityEntrance.setExit("N", cityCenter);
 
-const cityMarket = new Room("the player market", "");
+
+const cityMarket = new Room("Balial - Player Marketplace", ""); //"see list of open shops. max count is 60, type --- 2 to see the next page!"
 cityCenter.setExit("W", cityMarket);
 
-const cityExit = new Room("the city exit", "");
+
+const cityExit = new Room("Balial - Town Gates", ""); //introduce concept of server costs. sign that says the server is only the first starting town now to conserve memory
 cityCenter.setExit("E", cityExit);
 
 
-const tavern = new Room("the tavern", "a tavern");
+
+const tavern = new Room("Balial - The Red Flagon", "A tavern."); //call to adventure. this is where the adventure would have started, and had started for many people. signs, wanted posters, evidence of previous events. think aardwolf!
 cityCenter.setExit("N", tavern);
 
-const tavernRoom = new Room("tavern room", "");
+
+const tavernRoom = new Room("The Red Flagon - Storeroom", "An empty storeroom."); //interactivity. counter? candles? messages scratched into wall?
 tavern.setExit("E", tavernRoom);
 
 
